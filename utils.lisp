15292b0b408f3b9959aeae1502ca46ece79e220d
Karim Rasmus Vedelkvist <karim.vedelkvist@hotmail.com>
2025-12-01 11:56:56 +0100

    merge main master

--- utils.lisp
+++ utils.lisp
@@ -11,15 +11,3 @@
                       (format nil "day~2,'0d.txt" day))))
     (merge-pathnames filename
                      (merge-pathnames (format nil "~d/" year) *input-dir*))))
-
-(defun read-input (year day &key test)
-  "Read raw input for a year/day as a string."
-  (uiop:read-file-string (input-file year day :test test)))
-
-(defun read-lines (year day &key test)
-  "Read input as a list of lines."
-  (uiop:read-file-lines (input-file year day :test test)))
-
-(defun read-integers (year day &key test)
-  "Read input as a list of integers."
-  (mapcar #'parse-integer (read-lines year day :test test)))
