7917b9e5f381ba9972c673c0258c415a6e09bc12
Karim Rasmus Vedelkvist <karim.vedelkvist@hotmail.com>
2025-12-03 22:31:22 +0100

    compute length once pr. outer loop

--- src/2025/day03/solution.lisp
+++ src/2025/day03/solution.lisp
@@ -16,9 +16,10 @@
 (defun solve-part2 (input-file)
   "Solve part 2 of day 03."
   (loop for line in (read-input input-file)
+        for line-length = (length line)
         sum (loop for digits-left from 12 downto 1
                   for search-start = 0 then (1+ picked-pos)
-                  for search-end = (- (length line) digits-left)
+                  for search-end = (- line-length digits-left)
                   for best-digit = (loop for i from search-start to search-end
                                          maximize (digit-char-p (char line i)))
                   for picked-pos = (loop for i from search-start to search-end
